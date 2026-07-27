e185f7fface2ab2d9230b05b104ac3472489572e
bernamar2003 <benilsonsilva129@gmail.com>
2026-07-27 06:30:38 +0100

    Bug solucionado

--- Employee System/Main.java
+++ Employee System/Main.java
@@ -1,5 +1,27 @@
 public class Main {
 
+    static class Funcionario {
+        private String nome;
+        private String cargo;
+        private double salario;
+
+        public Funcionario(String nome, String cargo, double salario) {
+            this.nome = nome;
+            this.cargo = cargo;
+            this.salario = salario;
+        }
+
+        public void aumentarSalario(double percentual) {
+            this.salario += this.salario * percentual / 100;
+        }
+
+        public void exibirInformacoes() {
+            System.out.println("Nome: " + nome);
+            System.out.println("Cargo: " + cargo);
+            System.out.println("Salário: R$" + String.format("%.2f", salario));
+        }
+    }
+
     public static void main(String[] args) {
 
         // Criando dois funcionários
